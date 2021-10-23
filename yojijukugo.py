2c68f1d45a1b96a5fe1e030f2403f8362e040c13
neetcodes <72182918+neetcodes@users.noreply.github.com>
2021-10-23 16:27:23 +0300

    remove caching features
    no one needs them other than myself

--- yojijukugo.py
+++ yojijukugo.py
@@ -2,10 +2,13 @@
 import sys
 import os
 import os.path
+import requests
+from bs4 import BeautifulSoup
+import re
 
-cache = "/home/msg/.cache/yojijukugo/"
 useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
 def getentry(word, showreading=True):
+    # check if the input is in Japanese to eliminate unnecessary lookups
     japanese = False
     for char in word:
         o = ord(char)
@@ -20,36 +23,23 @@ def getentry(word, showreading=True):
     if not japanese:
         return("")
 
-    if os.path.isfile(cache + word):
-        with open(cache + word,'r') as f:
-            if showreading:
-                return(f.read())
-            else:
-                text = f.read()
-                return(text[text.find("\n"):])
-    else:
-        import requests
-        from bs4 import BeautifulSoup
-        import re
-        headers = requests.utils.default_headers()
-        headers.update({'User-Agent': useragent,})
-        url = "https://yoji.jitenon.jp/cat/search.php?getdata={}&search=contain".format(word)
-        try:
-            req = requests.get(url, headers=headers)
-        except:
-            print("lol")
-            return("")
-        soup = BeautifulSoup(req.text, 'lxml')
-        mainarea = soup.find("table", {"class": "kanjirighttb"})
-        if mainarea != None:
-            reading = mainarea.find("th", string="読み方").find_next_sibling("td").text.replace("\r","")
-            defin  = mainarea.find("th", string="意味").find_next_sibling("td").text.replace("\r","")
-            with open(cache + word, 'w') as f:
-                f.write(reading + "\n" + defin)
-            if showreading:
-                return(reading + "\n" + defin)
-            else:
-                return(defin)
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': useragent,})
+    url = "https://yoji.jitenon.jp/cat/search.php?getdata={}&search=contain".format(word)
+    try:
+        req = requests.get(url, headers=headers)
+    except:
+        print("lol")
+        return("")
+    soup = BeautifulSoup(req.text, 'lxml')
+    mainarea = soup.find("table", {"class": "kanjirighttb"})
+    if mainarea != None:
+        reading = mainarea.find("th", string="読み方").find_next_sibling("td").text.replace("\r","")
+        defin  = mainarea.find("th", string="意味").find_next_sibling("td").text.replace("\r","")
+        if showreading:
+            return(reading + "\n" + defin)
+        else:
+            return(defin)
     return("")
 
 if __name__ == "__main__":
